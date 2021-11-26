fb059deaddc4ab7a8e60f77abb167e61281fc733
suryanshsinhsisodiya <78161487+suryanshsinhsisodiya@users.noreply.github.com>
2021-11-26 15:22:33 +0530

    Delete LinkShortner.py

--- LinkShortner.py
+++
@@ -1,7 +0,0 @@
-import bitly_api
-user = "o_amodqqf77"
-api = "fadcdf709a043bc49d23f5fe438b55cb08ad7852"
-bitly = bitly_api.Connection(user, api)
-while True:
-    shortlink = bitly.shorten(input("Paste link: "))
-    print("Shortened:  ")
